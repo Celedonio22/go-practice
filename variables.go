ed094b372c4903ee3f6dc5961eeac58a1bcabaa1
eliud.miguel <eliud.miguel@intercase.com.mx>
2025-01-16 18:02:02 -0600

    variables practice added

---
+++ variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	var age int
+	var height float64
+	var isAdult bool
+	name = "Edward"
+	age = 28
+	height = 1.72
+	isAdult = true
+	fmt.Println(name, age, height, isAdult)
+}
